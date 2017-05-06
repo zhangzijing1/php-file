8a9570a5e8f66d271184c29935db0fff3b87c850
张子静 <690710360@qq.com>
2017-05-06 21:28:57 +0800

    提交PHP如何解决FILES获得、保存

---
+++ port2.php
@@ -0,0 +1,24 @@
+<?php
+if ($_FILES["file"]["error"] > 0)
+  {
+  echo "错误: " . $_FILES["file"]["error"] . "<br />";
+  }
+else
+  {
+  echo "文件名: " . $_FILES["file"]["name"] . "<br />";
+  echo "类型: " . $_FILES["file"]["type"] . "<br />";
+  echo "大小:" . ($_FILES["file"]["size"] / 1024) . " Kb<br />"; 
+  }
+ if (file_exists("upload/" . $_FILES["file"]["name"]))
+    {
+      echo $_FILES["file"]["name"] . " 文件已经存在. ";
+    }
+else
+    {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "upload/" . $_FILES["file"]["name"]);
+      echo "文件已经被存储到: " . "upload/" . $_FILES["file"]["name"];
+    }
+ 
+
+?>
